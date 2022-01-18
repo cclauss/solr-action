471244ddb89ce85a2d83bff456a25eca9e4019f2
patric.dosch <patric.dosch@virtual-identity.com>
2022-01-18 16:31:22 +0100

    Initial commit
    - fix if

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,18 @@
 
 docker_run="docker run"
 docker_run="$docker_run -d -p 8983:8983 solr:$INPUT_VERSION"
-if [ ! -z "$INPUT_CONFIG_PATH" ] then
+if [ ! -z "$INPUT_CONFIG_PATH" ]
+then
 docker_run="$docker_run -v $PWD/$INPUT_CONFIG_PATH:/myconfig:ro"
 fi
 
-if [ ! -z "$INPUT_CORE_NAME" ] then
+if [ ! -z "$INPUT_CORE_NAME" ]
+then
 docker_run="$docker_run solr-create -c $INPUT_CORE_NAME"
 fi
 
-if [ ! -z "$INPUT_CONFIG_PATH" ] then
+if [ ! -z "$INPUT_CONFIG_PATH" ]
+then
 docker_run="$docker_run -d /myconfig"
 fi
 
