430401e7265333f4ec7a019a123b12a21e573e08
patric.dosch <patric.dosch@virtual-identity.com>
2022-01-20 18:08:46 +0100

    try path (as in docu)

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ docker_run="docker run"
 docker_run="$docker_run -d -p 8983:8983 solr:$INPUT_VERSION"
 if [ ! -z "$INPUT_CONFIG_PATH" ]
 then
-docker_run="$docker_run -v $PWD/$INPUT_CONFIG_PATH:/myconfig:ro"
+docker_run="$docker_run -v $PWD/$INPUT_CONFIG_PATH:/opt/solr/server/solr/configsets/myconfig:ro"
 fi
 
 if [ ! -z "$INPUT_CORE_NAME" ]
@@ -14,7 +14,7 @@ fi
 
 if [ ! -z "$INPUT_CONFIG_PATH" ]
 then
-docker_run="$docker_run -d /myconfig"
+docker_run="$docker_run -d /opt/solr/server/solr/configsets/myconfig"
 fi
 
 sh -c "$docker_run"
