fa031294c484a5cca3f4cf9a2b77fd5412bda9d8
patric.dosch <patric.dosch@virtual-identity.com>
2022-01-20 18:16:11 +0100

    try /github/workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ docker_run="docker run"
 docker_run="$docker_run -d -p 8983:8983 solr:$INPUT_VERSION"
 if [ ! -z "$INPUT_CONFIG_PATH" ]
 then
-docker_run="$docker_run -v $PWD/$INPUT_CONFIG_PATH:/opt/solr/server/solr/configsets/myconfig:ro"
+# docker_run="$docker_run -v $PWD/$INPUT_CONFIG_PATH:/opt/solr/server/solr/configsets/myconfig:ro"
+# docker_run="$docker_run -v /github/workspace/$INPUT_CONFIG_PATH:/opt/solr/server/solr/configsets/myconfig:ro"
 fi
 
 if [ ! -z "$INPUT_CORE_NAME" ]
@@ -14,7 +15,8 @@ fi
 
 if [ ! -z "$INPUT_CONFIG_PATH" ]
 then
-docker_run="$docker_run -d /opt/solr/server/solr/configsets/myconfig"
+# docker_run="$docker_run -d /opt/solr/server/solr/configsets/myconfig"
+docker_run="$docker_run -d /github/workspace/$INPUT_CONFIG_PATH"
 fi
 
 sh -c "$docker_run"
