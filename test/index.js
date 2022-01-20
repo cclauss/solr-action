37214b128248a3de4d5243aad9253ab9c599a2b1
patric.dosch <patric.dosch@virtual-identity.com>
2022-01-20 14:40:25 +0100

    verify test with no solr - test node 12

--- test/index.js
+++ test/index.js
@@ -2,17 +2,14 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const solr = require('solr-client')
-const client = solr.createClient({core: 'core5'})
 
 async function run(){
-  const response = await client.ping().catch(error => {
-    throw new Error(error);
-  });
-  console.log(response)
+  const client = solr.createClient({core: 'core5'})
+  await client.ping();
 }
 
 run().catch((err) => {
   console.error(err)
-  core.setFailed(err.message);
+  // core.setFailed(err.message);
   process.exit(1)
 })
