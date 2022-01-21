07f394962f454907d32fa009b16f39698f339808
patric.dosch <patric.dosch@virtual-identity.com>
2022-01-21 10:50:37 +0100

    test fix path

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,10 @@ docker_run="docker run"
 docker_run="$docker_run -d -p 8983:8983 solr:$INPUT_VERSION"
 if [ ! -z "$INPUT_CONFIG_PATH" ]
 then
-REPO=`echo "$GITHUB_REPOSITORY" | cut -d "/" -f 2`
-INIT_SCRIPT_PATH="/home/runner/work/$REPO/$REPO/$INPUT_CONFIG_PATH"
-docker_run="$docker_run -v $INIT_SCRIPT_PATH:/myconfig"
+# REPO=`echo "$GITHUB_REPOSITORY" | cut -d "/" -f 2`
+# INIT_SCRIPT_PATH="/home/runner/work/$REPO/$REPO/$INPUT_CONFIG_PATH"
+# docker_run="$docker_run -v $INIT_SCRIPT_PATH:/myconfig"
+docker_run="$docker_run -v /home/runner/work/solr-action/solr-action/test/resources/solr/conf:/opt/solr/server/solr/configsets/myconfig"
 fi
 
 if [ ! -z "$INPUT_CORE_NAME" ]
@@ -16,9 +17,8 @@ fi
 
 if [ ! -z "$INPUT_CONFIG_PATH" ]
 then
-docker_run="$docker_run -d /myconfig"
+docker_run="$docker_run -d /opt/solr/server/solr/configsets/myconfig"
 fi
 
 echo "::set-output name=outttt::$docker_run"
-echo "$docker_run"
 sh -c "$docker_run"
